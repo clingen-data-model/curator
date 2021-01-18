9ea25880c1a3cd0817c8849d09bc5e1c6698f0d4
Tristan Nelson <thnelson@geisinger.edu>
2021-01-18 10:25:35 -0500

    Adding check for authorization
    Admin interface is WIP

--- src/curator/common/views.cljs
+++ src/curator/common/views.cljs
@@ -1,14 +1,19 @@
 (ns curator.common.views
   (:require [curator.common.events :as common-events]
             [curator.common.subs :as common-subs]
-            [re-frame.core :as re-frame :refer [subscribe dispatch]]))
+            [re-frame.core :as re-frame :refer [subscribe dispatch]]
+            [reitit.frontend.easy :as rfe]))
 
 (defn navbar []
-  (let [user @(subscribe [::common-subs/user])]
+  (let [user @(subscribe [::common-subs/user])
+        admin-user @(subscribe [::common-subs/user-is-admin])]
     [:nav.navbar {:role "navigation"}
      [:div.navbar-brand
-      [:div.navbar-item
+      [:a.navbar-item {:href (rfe/href :home)}
        [:img {:src "/images/logo.svg"}]]]
+     [:div.navbar-menu
+      (when admin-user
+        [:a.navbar-item {:href (rfe/href :admin)} "admin"])]
      (if user
        [:div.navbar-end
         [:div.navbar-item (:email user)]
--- src/curator/pages/home/views.cljs
+++ src/curator/pages/home/views.cljs
@@ -5,47 +5,67 @@
             [curator.common.subs :as common-subs]
             [curator.pages.home.events]))
 
-(defn home [] 
-  (let [suggested-genes [] ;;@(subscribe [::subs/suggested-genes])
-        topic @(subscribe [::subs/search-topic])]
-  [:div
-   [:section.section (common-views/navbar)]
-   [:section.section.home-search-main 
-    [:div.columns.has-text-centered
-     [:div.column
-      [:div.columns
-       [:div.column.is-one-fifth.is-offset-one-fifth
-        [:div.dropdown.is-hoverable
-         [:div.dropdown-trigger
-          [:button.button {:aria-haspopup "true" :aria-controls "dropdown-menu"}
-           [:span topic]
-           [:span.icon.is-small
-            [:i.fas.fa-angle-down {:aria-hidden "true"}]]]]
-         [:div.dropdown-menu {:id "dropdown-menu" :role "menu"}
-          [:div.dropdown-content.has-text-left
-           [:a.dropdown-item {:href "#"
-                              :on-click #(dispatch [:home/set-search-topic :gene])} 
-            "gene"]
-           [:a.dropdown-item {:href "#"
-                              :on-click #(dispatch [:home/set-search-topic :disease])} 
-            "disease"]
-           [:a.dropdown-item {:href "#"
-                              :on-click #(dispatch [:home/set-search-topic :affiliation])}
-            "affiliation"]]]]]
-       [:div.column.is-two-fifths
-        [:div.field
-         [:div.control
-          [:input.input {:id "search-box"
-                         :type "text"
-                         :placeholder topic
-                         :on-change #(dispatch [:home/request-suggestions
-                                                (-> % .-target .-value)])}]]]]
 
-       ]
-      [:p @(subscribe [::subs/errors])]
-      (for [gene suggested-genes]
-        [:div.block 
-         [:a ](:text gene)
-         (when (seq (:curations gene))
-           [:span.icon
-            [:i.fas.fa-star]])])]]]]))
+(defn search []
+  (let [topic @(subscribe [::subs/search-topic])
+        suggested-genes []]
+    [:section.section.home-search-main 
+     [:div.columns.has-text-centered
+      [:div.column
+       [:div.columns
+        [:div.column.is-one-fifth.is-offset-one-fifth
+         [:div.dropdown.is-hoverable
+          [:div.dropdown-trigger
+           [:button.button {:aria-haspopup "true" :aria-controls "dropdown-menu"}
+            [:span topic]
+            [:span.icon.is-small
+             [:i.fas.fa-angle-down {:aria-hidden "true"}]]]]
+          [:div.dropdown-menu {:id "dropdown-menu" :role "menu"}
+           [:div.dropdown-content.has-text-left
+            [:a.dropdown-item {:href "#"
+                               :on-click #(dispatch [:home/set-search-topic :gene])} 
+             "gene"]
+            [:a.dropdown-item {:href "#"
+                               :on-click #(dispatch [:home/set-search-topic :disease])} 
+             "disease"]
+            [:a.dropdown-item {:href "#"
+                               :on-click #(dispatch [:home/set-search-topic :affiliation])}
+             "affiliation"]]]]]
+        [:div.column.is-two-fifths
+         [:div.field
+          [:div.control
+           [:input.input {:id "search-box"
+                          :type "text"
+                          :placeholder topic
+                          :on-change #(dispatch [:home/request-suggestions
+                                                 (-> % .-target .-value)])}]]]]]
+       [:p @(subscribe [::subs/errors])]
+       (for [gene suggested-genes]
+         [:div.block 
+          [:a ](:text gene)
+          (when (seq (:curations gene))
+            [:span.icon
+             [:i.fas.fa-star]])])]]]))
+
+(defn unauthorized-user []
+    [:section.hero
+     [:div.hero-body
+      [:div.container
+       [:h1.title "You are not authorized to access this resource"]
+       [:h2.subtitle "Please contact an administrator to gain access if you believe this message is in error."]]]])
+
+(defn login-invitation []
+    [:section.hero
+     [:div.hero-body
+      [:div.container
+       [:h1.title "ClinGen admin interface"]
+       [:h2.subtitle "Please login to continue"]]]])
+
+(defn home [] 
+  (let [authorization @(subscribe [::common-subs/user-authorization])]
+    [:div
+     [:section.section (common-views/navbar)]
+     (case authorization
+       :authorized (search)
+       :unauthorized (unauthorized-user)
+       (login-invitation))]))
